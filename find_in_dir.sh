328b123f2fc2df25945ca5b664d24ef385ed963f
Chris Horn <hammerhorn@gmail.com>
2015-07-14 10:54:20 -0400

    Path modified; shell scripts added

--- find_in_dir.sh
+++ find_in_dir.sh
@@ -4,7 +4,7 @@ for i in *
   do
     if (grep "$1" "$i" > /dev/null)
       then
-        echo -n "$i"|~/bin/ulcat.sh
+        echo -n "$i"| ./ulcat.sh
         echo ": "
         nl "$i"| grep --color "$1"
     fi
