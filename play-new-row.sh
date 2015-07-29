fd4897185a644cf97db8b33a2a671efbad73538c
Chris Horn <hammerhorn@gmail.com>
2015-07-29 15:10:47 -0400

    delete extra files

--- play-new-row.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-./tonerow > tmp
-echo "Data written to 'tmp'."
-echo
-#read -p "Press a <ENTER> to continue."
-./choose_player.py
--- ulcat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-tput smul
-cat "$@"
-tput rmul
